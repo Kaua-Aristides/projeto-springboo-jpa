36e22431a41be09fb7cdaa0226331abe89024ee1
Kaua-Aristides <kauaaristide@gmail.com>
2026-07-01 15:58:33 -0300

    Exception handling - findById

---
+++ src/main/java/com/educandoweb/course/resources/exceptions/StandardError.java
@@ -0,0 +1,70 @@
+package com.educandoweb.course.resources.exceptions;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class StandardError implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+	private Instant timestamp;
+	private Integer status;
+	private String error;
+	private String message;
+	private String pach;
+	
+	public StandardError() {
+		
+	}
+
+	public StandardError(Instant timestamp, Integer status, String error, String message, String pach) {
+		super();
+		this.timestamp = timestamp;
+		this.status = status;
+		this.error = error;
+		this.message = message;
+		this.pach = pach;
+	}
+
+	public Instant getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Instant timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPach() {
+		return pach;
+	}
+
+	public void setPach(String pach) {
+		this.pach = pach;
+	}
+}
